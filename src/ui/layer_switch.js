b4b34b278ba1ea93d4cda9cfb91227b5089449b9
Stephen Mather <stephen@smathermather.com>
2014-02-20 12:26:24 -0500

    adding "satellite"

--- src/ui/layer_switch.js
+++ src/ui/layer_switch.js
@@ -2,16 +2,19 @@ module.exports = function(context) {
 
     return function(selection) {
 
-        var layers = [{ 
+        var layers = [
+        {
+            title: 'Leaf-off',
+            layer: L.mapbox.tileLayer('tmcw.map-j5fsp01s', {
+                detectRetina: true
+            })
+        },
+        { 
             title: 'Leaf-On',  
             layer: L.tileLayer('http://69.54.58.148:8080/tiles/osm_EPSG900913/{z}/{x}/{y}.png?origin=nw', {
                 tms: false })
-        },
-        { 
-            title: 'Leaf-Off',  
-            layer: L.tileLayer('http://69.54.58.148:8070/tiles/osm_EPSG900913/{z}/{x}/{y}.png?origin=nw', {
-                tms: false })
-        }];
+        }
+        ];
 
         var layerSwap = function(d) {
             var clicked = this instanceof d3.selection ? this.node() : this;
