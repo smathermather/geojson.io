36e9f84079e9f3dcef6ea8fd9444b2a7056e8550
Stephen Mather <stephen@smathermather.com>
2014-02-21 08:02:31 -0500

    adding OSIPII (I hope)

--- src/ui/layer_switch.js
+++ src/ui/layer_switch.js
@@ -10,6 +10,11 @@ module.exports = function(context) {
             })
         },
         { 
+            title: 'OSIP',  
+            layer: L.tileLayer('http://69.54.58.148:8090/tiles/osm_EPSG900913/{z}/{x}/{y}.png?origin=nw', {
+                tms: false })
+        },
+        { 
             title: 'Leaf-On',  
             layer: L.tileLayer('http://69.54.58.148:8080/tiles/osm_EPSG900913/{z}/{x}/{y}.png?origin=nw', {
                 tms: false })
